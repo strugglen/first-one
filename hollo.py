3c56dba9e70993bf91ca8951963ad7591a24abd6
strugglen <73323822+strugglen@users.noreply.github.com>
2020-12-03 09:06:31 +0800

    second

--- hollo.py
+++ hollo.py
@@ -1,2 +1,10 @@
-list1 = list ("happy")
-print(list1)
+def area(wid,hei):
+    return wid * hei
+
+def print_welcome(name):
+    print("welcom",name)
+
+print_welcome("RUN")
+w = 7
+h =5
+print("wid=", w ,"hei=",h, "area=",area(w,h))
